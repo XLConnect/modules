b17a29d144009f6aa76a7ac93441b96268affcfe
Gert-Jan van der Kamp <gjvdkamp@xlconnect.net>
2023-01-26 19:02:21 +0100

    fix for http.put and post

--- http.js
+++ http.js
@@ -6,7 +6,7 @@
  * @returns value
  */
 exports.get  = (uri, hds=null, auth=null) => JSON.parse(xlc.get(uri, hds, auth).Result)
-exports.post = (uri, content, hds=null, auth=null) => JSON.parse(xlc.post(uri, content, hds, auth).Result)
-exports.put  = (uri, content, hds=null, auth=null) => JSON.parse(xlc.put(uri, content, hds, auth).Result)
+exports.post = (uri, content, hds=null, auth=null) => JSON.parse(xlc.post(uri, JSON.stringify(content), hds, auth).Result)
+exports.put  = (uri, content, hds=null, auth=null) => JSON.parse(xlc.put(uri, JSON.stringify(content), hds, auth).Result)
 exports.delete = (uri, hds=null, auth=null) => JSON.parse(xlc.delete(uri, hds, auth).Result)
 
