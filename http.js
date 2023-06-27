ca24a68c27475d457706c8f04c2733af1cc85f0a
Gert-Jan van der Kamp <johndoe@example.comgjvdkamp@gmail.com>
2023-06-27 22:12:59 +0200

    rewrrote http for better user guidance

--- http.js
+++ http.js
@@ -1,12 +1,42 @@
-/** 
- * Grab JSON content from a URI and parse it to a javascript value
- * @param {string} uri 
- * @param {object} hds 
- * @param {string} auth 
- * @returns value
- */
-exports.get = (uri, hds = null, auth = null) => JSON.parse(xlc.get(uri, hds, auth).Result)
-exports.post = (uri, content, hds = null, auth = null) => JSON.parse(xlc.post(uri, JSON.stringify(content), hds, auth).Result)
-exports.put = (uri, content, hds = null, auth = null) => JSON.parse(xlc.put(uri, JSON.stringify(content), hds, auth).Result)
-exports.delete = (uri, hds = null, auth = null) => JSON.parse(xlc.delete(uri, hds, auth).Result)
+function head(uri, hds = null, auth = null){
+    let res = xlc.head(uri, hds, auth).Result
+    return JSON.parse(res)
+}
+
+function get(uri, hds = null, auth = null){
+    let res = xlc.get(uri, hds, auth).Result
+    return JSON.parse(res)
+}
+
+function post (uri, content, hds = null, auth = null){
+    let j = JSON.stringify(content)
+    let res = xlc.post(uri, j, hds, auth).Result
+    return JSON.parse(res)
+}
+
+function put (uri, content, hds = null, auth = null){
+    let j = JSON.stringify(content)
+    let res = xlc.put(uri, j, hds, auth).Result
+    return JSON.parse(res)
+}
+
+function patch (uri, content, hds = null, auth = null){
+    let j = JSON.stringify(content)
+    let res = xlc.patch(uri, j, hds, auth).Result
+    return JSON.parse(res)
+}
+
+// delete already exists as a reserved word in js
+function fdelete(uri, hds = null, auth = null){
+    let res = xlc.delete(uri, hds, auth).Result
+    return JSON.parse(res)
+}
+
+
+exports.get = get
+exports.put = put
+exports.post = post
+exports.patch = patch
+exports.delete = fdelete
+//exports.head = head
 
