f8f62c4aead52b2dcecf33afce59f2d613c644c4
Gert-Jan van der Kamp <johndoe@example.comgjvdkamp@gmail.com>
2023-06-30 09:26:05 +0200

    Improved commnets, removed empty

--- file.js
+++ file.js
@@ -3,24 +3,27 @@ function read(path){
     return JSON.parse(json)
 }
 
-function write(path, item){
+function write(path, item){    
+    // item: can be any javascript variable, string, int, object, array etc
     json = JSON.stringify(item)
     xlc.fileWrite(path, json)
 }
 
+// delete is reserved word in js, so added f for function
 function fdelete(path){
     xlc.fileDelete(path)
 }
 
 function list(path, filter='*.*', subfolders=false){
-    // path is the path (starting from the XLConnect Data folder) 
-    // filter can have be wildcards like '*.json' or '2023-??.json', null 
-    // subfolders specifies is the search should include all folders below path
+    // path: the path to list files from (starting from the XLConnect Data folder) 
+    // filter: can have be wildcards like '*.json' or '2023-??.json', null 
+    // subfolders: search includes all folders below path
     return xlc.fileList(path, filter, subfolders)
 }
 
 function folderDelete(path, force = false){
-    // for force = false, folder must be empty. Use true to delete the folder and it's contents 
+    // path: the path to list files from (starting from the XLConnect Data folder) 
+    // force: if false, folder must be empty to succeed. Use true to delete the folder and any contents 
     xlc.folderDelete(path, force)
 }
 
--- util.js
+++ util.js
@@ -8,5 +8,10 @@ function parseUnixDate(unixTimestamp) {
     return new Date(unixTimestamp * 1000);
 }
 
+function isoDate(year, month, day){
+	return new Date(Date.UTC(year,month-1, day)).toISOString().slice(0,10)
+}
+
 exports.parseExcelDate = parseExcelDate;
 exports.parseUnixDate = parseUnixDate;
+exports.isoDate = isoDate;
