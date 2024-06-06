46ef7d75c89955ecf796a9bf28f37a308930566c
gjvdkamp <gjvdkamp@gmail.com>
2024-06-06 13:27:22 +0200

    improved handling of empty replies

--- http.js
+++ http.js
@@ -11,30 +11,35 @@ function get(uri, hds = null, auth = null){
 function post (uri, content, hds = null, auth = null){
     let j = JSON.stringify(content)
     let res = xlc.post(uri, j, hds, auth).Result
-    return JSON.parse(res)
+    return parseResult(res)
 }
 
 function put (uri, content, hds = null, auth = null){
     let j = JSON.stringify(content)
     let res = xlc.put(uri, j, hds, auth).Result
-    return JSON.parse(res)
+    return parseResult(res)
 }
 
 function patch (uri, content, hds = null, auth = null){
     let j = JSON.stringify(content)
     let res = xlc.patch(uri, j, hds, auth).Result
-    return JSON.parse(res)
+    return parseResult(res)
 }
 
 // delete already exists as a reserved word in js
 function fdelete(uri, hds = null, auth = null){
     let res = xlc.delete(uri, hds, auth).Result
-    return JSON.parse(res)
+    return parseResult(res)
 }
 
 function http(verb, uri, content, hds, auth){
     let j = content ? JSON.stringify(content) : null
     let res = xlc.http(verb, uri, j, hds, auth).Result
+    return parseResult(res)
+}
+
+function parseResult(res){    
+    if(res.trim() === "") return "" // empty string does not parse as valid json (but should)
     return JSON.parse(res)
 }
 
