ff0fab734da2c231acfc8e017f788de55f7d7b55
gjvdkamp <gjvdkamp@gmail.com>
2024-10-22 09:46:22 +0200

    added brincr

---
+++ brincr.js
@@ -0,0 +1,35 @@
+"use sctrict";
+
+
+function getAllPages(api, limit = 200) {
+	
+	let result = []
+	let paramStart = api.includes('?') ? '&' : '?'
+	let uri = base + api + paramStart + 'limit=' + limit
+		
+	while(true){
+	    console.log(uri)
+		
+		response = http.http('GET', uri, null, null, auth)
+	
+		// rate limit
+		rem = Number(response.Headers['X-RateLimit-Remaining'])
+		//console.log(rem)
+		if(rem < 5){
+			xlc.setProgressMessage('5 sec pauze voor API Rate limit...')
+			xlc.sleep(5000) // 5 sec wachten 
+			xlc.setProgressMessage('Fetching data...')
+		}
+
+	    //let res = http.get(uri, null, auth)
+		let res = JSON.parse(response.Content)
+	    result.push(res.data)
+	    if(res.links?.next){
+	   		uri = res.links?.next
+	    } else {
+	        return result.flat()
+	    }
+	}
+}
+
+exports.getAllPages = getAllPages
