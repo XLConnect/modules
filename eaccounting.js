576445237478f115e11cffbac6ca839dd4b74256
gjvdkamp <gjvdkamp@gmail.com>
2025-10-03 15:01:01 +0200

    added wrappers to eAccounting

--- eaccounting.js
+++ eaccounting.js
@@ -14,9 +14,25 @@ function accounts(){
     return call('/v2/Accounts')
 }
 
+function accountTypes(){
+	return call('/v2/AccountTypes')
+}
+
 function accountBalances(date){
     let date2 = util.YYYY_MM_DD(date)
-    return call(`/v2/AccountBalances?date=${date2}`)
+    return call(`/v2/AccountBalances/${date2}`)
+}
+
+function articles(){
+	return call('/v2/Articles')
+}
+
+function companySettings(){
+	return call('/v2/CompanySettings')
+}
+
+function costCentres(){
+	return call('/v2/CostCentres')
 }
 
 function customers(){
@@ -65,7 +81,11 @@ function call(api){
 exports.README = README
 
 exports.accounts = accounts
+exports.accountTypes = accountTypes
 exports.accountBalances = accountBalances
+exports.articles = articles
+exports.companySettings = companySettings
+exports.costCentres = costCentres
 exports.customers = customers
 exports.customerInvoices = customerInvoices
 exports.supplierInvoices = supplierInvoices
