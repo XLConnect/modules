d0aa724695ce298fa2b17ad850143d41ba26dbea
gjvdkamp <gjvdkamp@gmail.com>
2025-10-03 14:51:58 +0200

    added wrapper functions

--- eaccounting.js
+++ eaccounting.js
@@ -1,9 +1,39 @@
+"use strict";
+
+const util = require('util.js')
+const http = require('http.js')
+
+const base = 'https://eaccountingapi.vismaonline.com'
+const auth = 'eaccounting' // TODO : implement named cookies 
+
 function README(){
 
 }
 
-const base = 'https://eaccountingapi.vismaonline.com'
-const auth = 'eaccounting'
+function accounts(){
+    return call('/v2/Accounts')
+}
+
+function accountBalances(date){
+    let date2 = util.YYYY_MM_DD(date)
+    return call(`/v2/AccountBalances?date=${date2}`)
+}
+
+function customers(){
+    return call('/v2/Customers')
+}
+
+function customerInvoices(){
+    return call('/v2/CustomerInvoices')
+}
+
+function suppliers(){
+    return call('/v2/Suppliers')
+}
+
+function supplierInvoices(){
+    return call('/v2/SupplierInvoices')
+}
 
 function call(api){
 	
@@ -16,25 +46,29 @@ function call(api){
 		let uri2 = base + api + firstSep + `$page=${page}&$pagesize=${pageSize}`
 		console.log(uri2)
 		let res = http.get(uri2, null, auth)	
-		if(res.Meta){ // paginated response , collect all pages 
+		if(res.Meta){
+            // paginated response , collect all pages 
 			pages.push(res.Data)
 			if(res.Meta.CurrentPage < res.Meta.TotalNumberOfPages){
 				page++			
 			} else {
 				break
 			}
-		} else { // not paginated, just return result 
+		} else { 
+            // not paginated, just return result 
 			return res
 		}		
 	}
 	return pages.flat()
 }
 
+exports.README = README
 
+exports.accounts = accounts
+exports.accountBalances = accountBalances
+exports.customers = customers
+exports.customerInvoices = customerInvoices
+exports.supplierInvoices = supplierInvoices
+exports.suppliers = suppliers
 
-
-
-exports = {
-    README,
-    call
-}
+exports.call = call
